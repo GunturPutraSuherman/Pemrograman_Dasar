643eea2ab0b752895eec7a548c6aa569435aaa79
Guntur Putra Suherman <111579039+GunturPutraSuherman@users.noreply.github.com>
2022-09-13 14:38:33 +0700

    Add files via upload

---
+++ Var.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int var1, var2, var3;
+	int tambah, kurang, kali, bagi;
+	char karakter;
+	var1 = 10;
+	var2 = 5;
+	cout<<"Angka 1 =" << var1<<endl;
+	cout<<"Angka 2 =" << var2<<endl;
+	tambah = var1 + var2;
+	cout<<"Hasil tambah =" << tambah<<endl;
+	kurang = var1 - var2;
+	cout<<"Hasil pengurangan =" << kurang<<endl;
+	kali = var1 * var2;
+	cout<<"Hasil kali =" << kali<<endl;
+	bagi = var1 / var2;
+	cout<<"Hasil bagi =" << bagi<<endl;
+}
