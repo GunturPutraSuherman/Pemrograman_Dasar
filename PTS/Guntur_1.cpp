96f20c92092947538b86463e7a13474f8eec3fbc
Guntur Putra Suherman <111579039+GunturPutraSuherman@users.noreply.github.com>
2022-09-19 18:28:53 +0700

    Add files via upload

---
+++ PTS/Guntur_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	cout <<"Nama : Guntur"<<endl;
+	cout <<"Kelas : X RPL 2"<<endl;
+	cout <<"Nilai : 85"<<endl;
+	int nilai = 85;
+	if (nilai > 78){
+		cout << "Lulus" << endl;
+	}else{
+		cout <<"Tidak Lulus";
+	}
+return 0;
+}
